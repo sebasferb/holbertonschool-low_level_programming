11cb94d3ffc434f2206a84fca556e6475136e47c
sebasferb <773@holbertonschool.com>
2019-02-06 21:13:44 +0000

    C program that prints using function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts (\"Programming is like building a multilingual puzzle);
+/**
+ * print usign puts
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
