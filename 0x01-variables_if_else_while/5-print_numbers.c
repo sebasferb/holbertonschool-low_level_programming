ab648505316826c422158f062c3247fc2b0a1ad6
sebasferb <773@holbertonschool.com>
2019-02-07 21:05:18 +0000

    prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Putchar: write a single character
+ *
+ * printf: produce output according to a format
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+
+putchar ('\n');
+return (0);
+}
