bda1fe2b638bc919399564a7f9b6df80af1475d4
sebasferb <773@holbertonschool.com>
2019-02-07 22:23:15 +0000

    numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Putchar: write a single character
+ *
+*/
+
+int main(void)
+{
+	int x;
+
+        for (x = 0; x <= 9; x++)
+	{
+		putchar ((x % 10) + '0');
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+return (0);
+}
