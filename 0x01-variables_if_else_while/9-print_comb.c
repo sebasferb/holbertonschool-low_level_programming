8db6db21b0ab3af6fa56e590b23c99ef4f24cdd6
sebasferb <773@holbertonschool.com>
2019-02-07 23:41:43 +0000

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Putchar: write a single character
+ *
+ * printf: produce output according to a format
+*/
+
+int main(void)
+{
+	int x;
+	int y=',';
+	int z='$';
+
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar ((x % 10) + '0');
+		if (y == ',' && x != 9)
+		{
+	putchar (y);
+		}
+		if (x == 9)
+		{
+			putchar (z);
+		}
+	}
+
+putchar ('\n');
+return (0);
+}
