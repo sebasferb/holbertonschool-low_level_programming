2fc9fd67f2049a0d1954dc5eb5c941e9b4fd3bb5
sebasferb <773@holbertonschool.com>
2019-02-08 22:53:06 +0000

    all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <ctype.h>
-
 /**
  * main - Entry point
  *
@@ -9,26 +8,26 @@
  * Putchar: write a single character
  *
  * printf: produce output according to a format
-*/
+ */
 
 int main(void)
 {
 	int x;
 	int y;
-	
+
 	for (x = 48; x <= 57; x++)
 	{
 		for (y = x + 1; y <= 57; y++)
 		{
 			putchar(x);
 			putchar (y);
-			if( x != '8' || y != '9')
-			putchar (',');
+			if (x != '8' || y != '9')
+				putchar (',');
 			putchar (' ');
 		}
 
 	}
 
-putchar ('\n');
-return (0);
+	putchar ('\n');
+	return (0);
 }
