f606fe0f3af1a4cbb1f3b052dcc6603d887405fd
sebasferb <773@holbertonschool.com>
2019-02-08 16:26:58 +0000

    prints using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	const char *str = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	write(1, str, strlen (str));
+return (1);
+}
